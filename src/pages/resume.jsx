3122606ac06f319bb0e3d1ccf723f42b462e9c4f
Brayden Yip <yipb@uoguelph.ca>
2023-05-13 21:49:48 -0400

    change url of resume

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -3,7 +3,7 @@
 export default function Resume() {
     return (
         <div>
-            <object data="../assets/resume.pdf" type="application/pdf" width="200%" height="350%"/>
+            <object data="braydenyip.github.io/src/assets/resume.pdf" type="application/pdf" width="200%" height="350%"/>
         </div>
         
     )
